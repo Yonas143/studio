bc5bd555065d1a24b4a5b1cb78276d01ca4ba373
Yonas143 <yoniwin.yw@gmail.com>
2025-12-12 13:38:56 +0300

    test

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -29,6 +29,14 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.version': JSON.stringify('v18.17.0'),
+      })
+    );
+    return config;
   },
 };
 
